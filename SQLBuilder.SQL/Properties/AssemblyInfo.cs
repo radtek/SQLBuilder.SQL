4c7c068663ed152590104a81fbb39b6f83ce5ae7
trickjoseph <patrickjosephquijano@gmail.com>
2017-01-24 21:21:07 +0800

    Updated description.

--- SQLBuilder.SQL/Properties/AssemblyInfo.cs
+++ SQLBuilder.SQL/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SQLBuilder.SQL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A query builder for SQL.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Convergys Corporation")]
 [assembly: AssemblyProduct("SQLBuilder.SQL")]
